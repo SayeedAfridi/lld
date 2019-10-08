40bc67de5aab77cc7bb6ededb09ccc6ab21a154c
Sayeed Afridi <as.afridi563@gmail.com>
2019-10-09 01:07:58 +0600

    My Downloaded course List

---
+++ mycourses.py
@@ -0,0 +1,72 @@
+###These are my downloaded Courses 
+
+COURSES = [
+
+
+#*** Become a Vanilla JavaScript Developer ***
+    #'learning-app-building-with-vanilla-javascript',
+    #'web-portfolio-projects-binding-and-propagation',
+    #'learning-functional-programming-with-javascript-es6-plus',
+    #'building-web-applications-with-ajax',
+    #'processing-and-interchanging-json-data',
+    #'javascript-modern-browser-apis',
+    #'vanilla-javascript-service-workers',
+    #'vanilla-javascript-progressive-web-applications',
+
+#*** Become a programmer: Foundations ***
+        #'programming-foundations-fundamentals-3',
+        #'programming-foundations-object-oriented-design-3',
+        #'programming-foundations-algorithms',
+        #'programming-foundations-design-patterns-2',
+        #'programming-foundations-data-structures-2',
+        #'programming-foundations-databases-2',
+        #'programming-foundations-apis-and-web-services',
+        #'programming-foundations-web-security-2',
+        #'programming-foundations-test-driven-development-2',
+        #'programming-foundations-software-testing-qa',
+        #'programming-foundations-secure-coding',
+        #'programming-foundations-fuzzy-logic',
+
+
+#*** Become a Django Developer ***
+        #'learning-django',
+        #'building-a-personal-portfolio-with-django',
+        #'test-driven-development-in-django',
+        #'django-forms',
+        #'building-restful-web-apis-with-django',
+        #'deploying-django-apps-make-your-site-go-live',
+
+#**********For MEAN Stack ****
+       #'angular-essential-training-2',
+        #'building-angular-and-node-apps-with-authentication',
+
+#*** Become A React Native Developer ***
+        #'learning-react-native-2',
+        #'react-native-essential-training',
+        #'create-a-crm-mobile-application-with-react-native-2',
+        #'react-native-ecosystem-and-workflow',
+        #'building-material-design-apps-on-android-with-react-native',
+
+#********** Become a React Developer*****
+        #'learning-react-js-4'
+        #'react-js-essential-training-3'
+        #'learning-redux',
+        #'react-js-building-an-interface-2',
+        #'react-creating-and-hosting-a-full-stack-site',
+        #'react-ecosystems',
+
+#******* Learning path: Become a MERN Stack JavaScript Developer ****
+        #'learning-npm-the-node-package-manager-3',
+        #'node-js-essential-training-3',
+        #'building-a-website-with-node-js-and-express-js-2',
+        #'learning-mongodb',
+        #'express-essential-training',
+        #'building-restful-web-apis-with-node-js-and-express',
+        #'learning-full-stack-javascript-development-mongodb-node-and-react',
+        #'node-js-securing-restful-apis',
+        #'node-js-security',
+        #'react-securing-applications',
+        #'node-js-testing-and-code-quality',
+        #'node-js-deploying-applications',
+        #'react-js-essential-training-3'
+]
